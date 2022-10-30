8e50a14fcb51d81a7c66e11bd698878e2bb2b56e
darashi <darashi003@gmail.com>
2022-10-30 17:58:35 +0300

    Initial commit

---
+++ app/src/main/java/com/shinkevich/lab5_task4_java_shinkevich/MainActivity.java
@@ -0,0 +1,14 @@
+package com.shinkevich.lab5_task4_java_shinkevich;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
